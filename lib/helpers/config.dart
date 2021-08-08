8fc0a6d1b0f6fc9ec70aeddcc8b35873962e9f80
Daniele Ricci <daniele@casaricci.it>
2021-08-08 18:44:05 +0200

    Preparing for multiple aircrafts support
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- lib/helpers/config.dart
+++ lib/helpers/config.dart
@@ -7,56 +7,111 @@ import 'package:timezone/standalone.dart' as tz;
 
 class AppConfig {
 
+  @deprecated
   late String _googleServiceAccountJson;
   late SharedPreferences _prefs;
 
+  AircraftData? currentAircraft;
+
   init() async {
     _prefs = await SharedPreferences.getInstance();
+    // TEMP
     await dotenv.load(fileName: 'assets/data/.env');
-
     _googleServiceAccountJson =
         await rootBundle.loadString(dotenv.env['google_api_service_account']!);
+    currentAircraft = aircrafts![0];
 
     tzData.initializeTimeZones();
   }
 
   bool get admin {
-    return dotenv.env['admin']!.toLowerCase() == 'true';
+    return currentAircraft!.admin;
   }
 
   String get googleServiceAccountJson {
-    return _googleServiceAccountJson;
+    return currentAircraft!.backendInfo['google_api_service_account']!;
   }
 
   String get googleApiKey {
-    return dotenv.env['google_api_key']!;
+    return currentAircraft!.backendInfo['google_api_key']!;
   }
 
   String get googleCalendarId {
-    return dotenv.env['google_calendar_id']!;
+    return currentAircraft!.backendInfo['google_calendar_id']!;
   }
 
   double get locationLatitude {
-    return double.parse(dotenv.env['location_latitude']!);
+    return currentAircraft!.locationLatitude;
   }
 
   double get locationLongitude {
-    return double.parse(dotenv.env['location_longitude']!);
+    return currentAircraft!.locationLongitude;
   }
 
   tz.Location get locationTimeZone {
-    return tz.getLocation(dotenv.env['location_timezone']!);
+    return tz.getLocation(currentAircraft!.locationTimeZone);
   }
 
   // TODO handle non-pilot user "(prove tecniche)"
   List<String> get pilotNames {
-    return dotenv.env['pilotNames']!.split(r',');
+    return currentAircraft!.pilotNames;
   }
 
   ImageProvider getPilotAvatar(String name) {
+    // TODO load from files
     return AssetImage('assets/data/avatar-' + name.toLowerCase() + '.jpg');
   }
 
+  String? get _currentAircraftId {
+    return _prefs.getString('currentAircraft');
+  }
+
+  set _currentAircraftId(String? value) {
+    if (value != null) {
+      _prefs.setString('currentAircraft', value);
+    }
+    else {
+      _prefs.remove('currentAircraft');
+    }
+  }
+
+  void switchAircraft(AircraftData data) {
+    this.currentAircraft = data;
+    _currentAircraftId = data.id;
+  }
+
+  List<AircraftData>? get aircrafts {
+    // TODO
+
+    return [
+      AircraftData(
+        id: dotenv.env['aircraft_id']!,
+        callSign: dotenv.env['callsign']!,
+        backendInfo: {
+          'google_api_service_account': _googleServiceAccountJson,
+          'google_api_key': dotenv.env['google_api_key']!,
+          'google_calendar_id': dotenv.env['google_calendar_id']!,
+        },
+        pilotNames: dotenv.env['pilotNames']!.split(r','),
+        locationLatitude: double.parse(dotenv.env['location_latitude']!),
+        locationLongitude: double.parse(dotenv.env['location_longitude']!),
+        locationTimeZone: dotenv.env['location_timezone']!,
+        admin: dotenv.env['admin']!.toLowerCase() == 'true',
+      )
+    ];
+  }
+
+  void addAircraft(AircraftData data) {
+    // TODO
+  }
+
+  void updateAircraft(AircraftData data) {
+    // TODO
+  }
+
+  void removeAircraft(String id) {
+    // TODO
+  }
 
   String? get pilotName {
     return _prefs.getString('pilotName');
@@ -70,5 +125,28 @@ class AppConfig {
       _prefs.remove('pilotName');
     }
   }
+
+}
+
+class AircraftData {
+  final String id;
+  final String callSign;
+  final Map<String, dynamic> backendInfo;
+  final List<String> pilotNames;
+  final double locationLatitude;
+  final double locationLongitude;
+  final String locationTimeZone;
+  final bool admin;
+
+  AircraftData({
+    required this.id,
+    required this.callSign,
+    required this.backendInfo,
+    required this.pilotNames,
+    required this.locationLatitude,
+    required this.locationLongitude,
+    required this.locationTimeZone,
+    this.admin = false,
+  });
 
 }

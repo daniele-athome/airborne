ea4ae83ffedbdadc4e302ddc6b8f8993ab35acc2
Daniele Ricci <daniele@casaricci.it>
2021-09-02 23:55:06 +0200

    Testing widgets (WIP)
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

---
+++ test/screens/aircraft_select/aircraft_data_screen_test.dart
@@ -0,0 +1,53 @@
+
+import 'package:airborne/helpers/config.dart';
+import 'package:airborne/screens/aircraft_select/aircraft_data_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:provider/provider.dart';
+
+import 'aircraft_data_screen_test.mocks.dart';
+
+@GenerateMocks([AppConfig])
+void main() {
+  Widget createSkeletonApp() => MaterialApp(
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        locale: const Locale('en'),
+        home: _provideAppConfigForSampleAircraft(SetAircraftDataScreen())
+    );
+
+  group('Single aircraft data screen tests', () {
+    // TODO
+    testWidgets('An invalid URI in address field should not validate', (tester) async {
+      await tester.pumpWidget(createSkeletonApp());
+      await tester.enterText(
+          find.byWidgetPredicate((widget) => widget is PlatformTextFormField &&
+              widget.keyboardType == TextInputType.url), "NOT_VALID_URI");
+      await tester.tap(
+          find.byWidgetPredicate((widget) => widget is PlatformButton &&
+              // TODO localized string?
+              (widget.child! as Text).data == 'Install')
+      );
+      await tester.pumpAndSettle();
+      expect(tester.state<FormState>(find.byType(Form)).validate(), false);
+    });
+  });
+}
+
+Provider<AppConfig> _provideAppConfigForSampleAircraft(Widget child) {
+  final appConfig = MockAppConfig();
+  // TODO stub some stuff
+  return Provider.value(
+    value: appConfig,
+    builder: (context, __) => child,
+  );
+}

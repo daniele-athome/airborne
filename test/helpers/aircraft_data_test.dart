462ecfdf0c59bd6bda0269ce4dfdf5dd0588d780
Daniele Ricci <daniele@casaricci.it>
2021-09-02 21:04:39 +0200

    Testing stuff (WIP)
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- test/helpers/aircraft_data_test.dart
+++ test/helpers/aircraft_data_test.dart
@@ -4,8 +4,11 @@ import 'dart:math';
 import 'package:airborne/helpers/aircraft_data.dart';
 import 'package:archive/archive.dart';
 import 'package:archive/archive_io.dart';
+import 'package:mockito/mockito.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
+import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -20,19 +23,19 @@ void main() {
     });
 
     test('A zip file with an invalid aircraft JSON file should not pass validation', () async {
-      final badZipFile = await _createAircraftFileWithData(jsonData: '{3723;.-\\||}');
+      final badZipFile = await _createAircraftFileWithData(filenameWithoutExtension: 'a1234', jsonData: '{3723;.-\\||}');
       final reader = AircraftDataReader(dataFile: badZipFile);
       expect(await reader.validate(), false);
     });
 
     test('A zip file with an aircraft JSON file missing stuff should not pass validation', () async {
-      final badZipFile = await _createAircraftFileWithData(jsonData: '{"aircraft_id":"a1234","callsign":"A-1234"}');
+      final badZipFile = await _createAircraftFileWithData(filenameWithoutExtension: 'a1234', jsonData: '{"aircraft_id":"a1234","callsign":"A-1234"}');
       final reader = AircraftDataReader(dataFile: badZipFile);
       expect(await reader.validate(), false);
     });
 
     test('A zip file with a valid aircraft JSON file should pass validation', () async {
-      final badZipFile = await _createAircraftFileWithData(jsonData: '''
+      final goodZipFile = await _createAircraftFileWithData(filenameWithoutExtension: 'a1234', jsonData: '''
 {
   "admin": true,
   "aircraft_id": "a1234",
@@ -55,7 +58,7 @@ void main() {
     "timezone": "Europe/Berlin"
   }
 }''');
-      final reader = AircraftDataReader(dataFile: badZipFile);
+      final reader = AircraftDataReader(dataFile: goodZipFile);
       expect(await reader.validate(), true);
     });
 
@@ -63,17 +66,106 @@ void main() {
   });
 
   group('Testing aircraft data file opening', () {
-    // TODO
+    setUp(() {
+      Directory('.testdata').deleteSync(recursive: true);
+      PathProviderPlatform.instance = MockPathProviderPlatform();
+    });
+
+    // TODO some tests for bad cases here would be nice
+
+    test('Aircraft data should exist in temp directory after open', () async {
+      final goodZipFile = await _createExampleValidAircraftData();
+      final reader = AircraftDataReader(dataFile: goodZipFile);
+      final baseDir = await getTemporaryDirectory();
+      final directory = Directory(path.join(baseDir.path, 'aircrafts', 'a1234'));
+      final actual = await reader.open();
+      expect(actual.path, directory.path);
+      expect(directory.existsSync(), true);
+      expect(File(path.join(directory.path, 'aircraft.json')).existsSync(), true);
+      expect(File(path.join(directory.path, 'aircraft.jpg')).existsSync(), true);
+      expect(File(path.join(directory.path, 'avatar-anna.jpg')).existsSync(), true);
+      expect(File(path.join(directory.path, 'avatar-claudia.jpg')).existsSync(), true);
+      expect(File(path.join(directory.path, 'avatar-john.jpg')).existsSync(), true);
+      expect(File(path.join(directory.path, 'avatar-mike.jpg')).existsSync(), true);
+      expect(File(path.join(directory.path, 'avatar-simon.jpg')).existsSync(), true);
+    });
+
+    test('Aircraft data should be stored as new aircraft', () async {
+      final goodZipFile = await _createExampleValidAircraftData();
+      final reader = AircraftDataReader(dataFile: goodZipFile);
+      await reader.open();
+      final storedFile = await addAircraftDataFile(reader);
+      expect(storedFile.path, '.testdata/appdata/aircrafts/a1234.zip');
+      expect(File('.testdata/appdata/aircrafts/a1234.zip').existsSync(), true);
+    });
+
+    test('Loading existing aircraft should extract data in temp directory', () async {
+      final goodZipFile = await _createExampleValidAircraftData();
+      final reader = AircraftDataReader(dataFile: goodZipFile);
+      await reader.open();
+      await addAircraftDataFile(reader);
+
+      final loadedReader = await loadAircraft('a1234');
+      final baseDir = await getTemporaryDirectory();
+      final directory = Directory(path.join(baseDir.path, 'aircrafts', 'a1234'));
+      final actual = await loadedReader.open();
+      expect(actual.path, directory.path);
+      expect(directory.existsSync(), true);
+      expect(File(path.join(directory.path, 'aircraft.json')).existsSync(), true);
+      expect(File(path.join(directory.path, 'aircraft.jpg')).existsSync(), true);
+      expect(File(path.join(directory.path, 'avatar-anna.jpg')).existsSync(), true);
+      expect(File(path.join(directory.path, 'avatar-claudia.jpg')).existsSync(), true);
+      expect(File(path.join(directory.path, 'avatar-john.jpg')).existsSync(), true);
+      expect(File(path.join(directory.path, 'avatar-mike.jpg')).existsSync(), true);
+      expect(File(path.join(directory.path, 'avatar-simon.jpg')).existsSync(), true);
+    });
   });
+
+}
+
+Future<File> _createExampleValidAircraftData() {
+  return _createAircraftFileWithData(filenameWithoutExtension: 'a1234', jsonData: '''
+{
+  "admin": true,
+  "aircraft_id": "a1234",
+  "callsign": "A-1234",
+  "backend_info": {
+    "google_api_service_account": "BLABLABLA",
+    "google_api_key": "API_KEY_NONE",
+    "google_calendar_id": "NO_CALENDAR_MAN"
+  },
+  "pilot_names": [
+    "Mike",
+    "John",
+    "Claudia",
+    "Anna",
+    "Simon"
+  ],
+  "location": {
+    "latitude": 52.8844253,
+    "longitude": 12.7143166,
+    "timezone": "Europe/Berlin"
+  }
+}''',
+    aircraftPicData: List<int>.filled(1000, 0x0A),
+    pilotAvatarsPicData: {
+      "anna": List<int>.filled(1000, 0x0A),
+      "claudia": List<int>.filled(1000, 0x0A),
+      "john": List<int>.filled(1000, 0x0A),
+      "mike": List<int>.filled(1000, 0x0A),
+      "simon": List<int>.filled(1000, 0x0A),
+    },
+  );
 }
 
 Future<File> _createAircraftFileWithData({
+    required String filenameWithoutExtension,
     String? jsonData,
     List<int>? aircraftPicData,
     Map<String, List<int>>? pilotAvatarsPicData,
   }) async {
   final tmpDir = await getTemporaryDirectory();
-  final zipFile = File(path.join(tmpDir.path, 'aircraft_test_${Random().nextInt(1000)}.zip'));
+  final zipFile = File(path.join(tmpDir.path, '$filenameWithoutExtension.zip'));
 
   final encoder = ZipEncoder();
   encoder.startEncode(OutputFileStream(zipFile.path));
@@ -96,4 +188,46 @@ Future<File> _createAircraftFileWithData({
   encoder.endEncode();
 
   return zipFile;
+}
+
+class MockPathProviderPlatform extends Mock
+    with MockPlatformInterfaceMixin
+    implements PathProviderPlatform {
+
+  final String baseDir = '.testdata';
+
+  @override
+  Future<String> getTemporaryPath() async {
+    return path.join(baseDir, 'temp');
+  }
+
+  @override
+  Future<String> getApplicationSupportPath() async {
+    return path.join(baseDir, 'appdata');
+  }
+
+  @override
+  Future<String> getLibraryPath() async {
+    return path.join(baseDir, 'lib');
+  }
+
+  @override
+  Future<String> getApplicationDocumentsPath() async {
+    return path.join(baseDir, 'docs');
+  }
+
+  @override
+  Future<String> getExternalStoragePath() async {
+    return path.join(baseDir, 'ext');
+  }
+
+  @override
+  Future<List<String>> getExternalCachePaths() async {
+    return [];
+  }
+
+  @override
+  Future<String> getDownloadsPath() async {
+    return path.join(baseDir, 'download');
+  }
 }

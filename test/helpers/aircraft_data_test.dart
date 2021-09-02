8ce3d830585ae19661921be168911252471a679f
Daniele Ricci <daniele@casaricci.it>
2021-09-02 23:37:25 +0200

    Debug macOS build issue
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- test/helpers/aircraft_data_test.dart
+++ test/helpers/aircraft_data_test.dart
@@ -67,9 +67,11 @@ void main() {
 
   group('Testing aircraft data file opening', () {
     setUp(() {
-      print('SETTING UP MOCK PATH PROVIDER');
       PathProviderPlatform.instance = MockPathProviderPlatform();
-      print('CURRENT PATH PROVIDER: ${PathProviderPlatform.instance}');
+      // This is required because we manually register the Linux path provider when on the Linux platform.
+      // Will be removed when automatic registration of dart plugins is implemented.
+      // See this issue https://github.com/flutter/flutter/issues/52267 for details
+      disablePathProviderPlatformOverride = true;
     });
 
     // TODO some tests for bad cases here would be nice

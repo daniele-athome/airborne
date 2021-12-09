5c0293a7e2f2c463c0d497b1b3eb0debeeb0b4ad
Daniele Ricci <daniele@casaricci.it>
2021-12-09 22:26:35 +0100

    Flight log create and update services
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- lib/helpers/googleapis.dart
+++ lib/helpers/googleapis.dart
@@ -92,17 +92,19 @@ class GoogleSheetsService {
   }
 
   Future<AppendValuesResponse> appendRows(String spreadsheetId, String sheetName, String range, List<List<Object?>> values) {
+    final _range = sheetRange(sheetName, range);
     return _api.spreadsheets.values.append(ValueRange(
-      range: sheetRange(sheetName, range),
+      range: _range,
       values: values,
-    ), spreadsheetId, range, valueInputOption: 'USER_ENTERED');
+    ), spreadsheetId, _range, valueInputOption: 'USER_ENTERED');
   }
 
   Future<UpdateValuesResponse> updateRows(String spreadsheetId, String sheetName, String range, List<List<Object?>> values) {
+    final _range = sheetRange(sheetName, range);
     return _api.spreadsheets.values.update(ValueRange(
-      range: sheetRange(sheetName, range),
+      range: _range,
       values: values,
-    ), spreadsheetId, range, valueInputOption: 'USER_ENTERED');
+    ), spreadsheetId, _range, valueInputOption: 'USER_ENTERED');
   }
 
   Future<BatchUpdateSpreadsheetResponse> deleteRows(String spreadsheetId, String sheetName, int startRow, int endRow) {

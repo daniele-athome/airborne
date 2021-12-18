d705d9d35e1372c4d63682384eb1fbcba4924412
Daniele Ricci <daniele@casaricci.it>
2021-12-18 09:09:20 +0100

    Proper test data
    Google APIs will need mocking though.
    
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- integration_test/aircraft_data.sh
+++ integration_test/aircraft_data.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-INPUT="$1"
-
 cat <<EOF
 // Automatically generated. DO NOT EDIT.
 import 'dart:typed_data';
@@ -9,7 +7,7 @@ import 'dart:typed_data';
 final kTestAircraftData = Uint8List.fromList(<int>[
 EOF
 
-hexdump -e '16/1 "0x%02x_" "\n"' "$INPUT" | sed 's/0x  _//g'| sed 's/_/, /g; s/.*/    &/'
+hexdump -e '16/1 "0x%02x_" "\n"' | sed 's/0x  _//g'| sed 's/_/, /g; s/.*/    &/'
 
 cat <<EOF
 ]);

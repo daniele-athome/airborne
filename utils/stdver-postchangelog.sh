dee6ca9ff7d630a8f2f9ab54be7a1c8776d12e77
Daniele Ricci <daniele@casaricci.it>
2022-01-29 21:18:04 +0100

    Prepare for what's new generation (#48)
    [skip ci]
    
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

---
+++ utils/stdver-postchangelog.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Called as postchangelog script by standard-version.
+# Used to copy the contents of the latest version in Fastlane what's new metadata files.
+
+# TODO extract changes from latest version (which ones?)

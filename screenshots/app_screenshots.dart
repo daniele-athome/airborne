08f622ece30cdf3740b526de6da22cc6ae99df0c
Daniele Ricci <daniele@casaricci.it>
2023-03-05 21:41:17 +0100

    chore: unused code

--- screenshots/app_screenshots.dart
+++ screenshots/app_screenshots.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:airborne/helpers/config.dart';
 import 'package:airborne/helpers/utils.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';

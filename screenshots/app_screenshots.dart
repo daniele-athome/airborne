66107556a28c0adef65fc922525b2ede95730ce6
Daniele Ricci <daniele@casaricci.it>
2025-08-22 16:40:11 +0200

    wip: try again with screenshots package [skip ci]

--- screenshots/app_screenshots.dart
+++ screenshots/app_screenshots.dart
@@ -1,15 +1,13 @@
 import 'dart:io';
-import 'dart:ui';
 
 import 'package:airborne/helpers/config.dart';
 import 'package:airborne/helpers/utils.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:integration_test/src/channel.dart' show integrationTestChannel;
 import 'package:intl/intl_standalone.dart';
 import 'package:provider/provider.dart';
+import 'package:screenshots/src/capture_screen.dart';
 import 'package:timezone/data/latest.dart' as tz_data;
 
 import 'fake_data.dart';
@@ -62,7 +60,7 @@ void main() async {
     testWidgets('Onboarding - Select pilot', (WidgetTester tester) async {
       runApp(await appMain());
       await tester.pumpAndSettle();
-      await screenshot(binding, tester, locale, deviceName, orientation, '50-onboarding-pilotselect');
+      await screenshot(binding, tester, '50-onboarding-pilotselect');
     });
 
     testWidgets('Book flight - Agenda view', (WidgetTester tester) async {
@@ -70,7 +68,7 @@ void main() async {
       await tester.pumpAndSettle();
       await tester.tap(find.byKey(const Key('button_bookFlight_view_schedule')));
       await tester.pumpAndSettle();
-      await screenshot(binding, tester, locale, deviceName, orientation, '01-bookflight-agenda');
+      await screenshot(binding, tester, '01-bookflight-agenda');
     });
 
     testWidgets('Book flight - Month view', (WidgetTester tester) async {
@@ -78,7 +76,7 @@ void main() async {
       await tester.pumpAndSettle();
       await tester.tap(find.byKey(const Key('button_bookFlight_view_month')));
       await tester.pumpAndSettle();
-      await screenshot(binding, tester, locale, deviceName, orientation, '02-bookflight-month');
+      await screenshot(binding, tester, '02-bookflight-month');
     });
 
     testWidgets('Book flight - Flight editor', (WidgetTester tester) async {
@@ -86,7 +84,7 @@ void main() async {
       await tester.pumpAndSettle();
       await tester.tap(find.byKey(const Key('button_bookFlight')));
       await tester.pumpAndSettle();
-      await screenshot(binding, tester, locale, deviceName, orientation, '03-bookflight-flighteditor');
+      await screenshot(binding, tester, '03-bookflight-flighteditor');
     });
 
     testWidgets('Log book - List view', (WidgetTester tester) async {
@@ -94,7 +92,7 @@ void main() async {
       await tester.pumpAndSettle();
       await tester.tap(find.byKey(const Key('nav_flight_log')));
       await tester.pumpAndSettle();
-      await screenshot(binding, tester, locale, deviceName, orientation, '04-logbook-list');
+      await screenshot(binding, tester, '04-logbook-list');
     });
 
     testWidgets('Log book - Flight editor', (WidgetTester tester) async {
@@ -104,7 +102,7 @@ void main() async {
       await tester.pumpAndSettle();
       await tester.tap(find.byKey(const Key('button_logFlight')));
       await tester.pumpAndSettle();
-      await screenshot(binding, tester, locale, deviceName, orientation, '05-logbook-flighteditor');
+      await screenshot(binding, tester, '05-logbook-flighteditor');
     });
 
     testWidgets('Activities - List view', (WidgetTester tester) async {
@@ -112,63 +110,7 @@ void main() async {
       await tester.pumpAndSettle();
       await tester.tap(find.byKey(const Key('nav_activities')));
       await tester.pumpAndSettle();
-      await screenshot(binding, tester, locale, deviceName, orientation, '06-activities-list');
+      await screenshot(binding, tester, '06-activities-list');
     });
   });
-}
-
-/// We cannot import integration_test or flutter_test because they lead to the following error: 'dart:ui' not found
-/// More info here: https://github.com/flutter/flutter/issues/27826
-/// Make sure to pass a IntegrationTestWidgetsFlutterBinding for `binding` and a WidgetTester for `tester`
-Future screenshot(dynamic binding, dynamic tester, String locale, String deviceName, String orientation, String name) async {
-  if (Platform.isAndroid) {
-    // This is required prior to taking the screenshot (Android only).
-    await binding.convertFlutterSurfaceToImage();
-  }
-
-  // Trigger a frame.
-  await tester.pumpAndSettle();
-
-  final String platform;
-  if (Platform.isIOS) {
-    platform = "ios";
-  }
-  // TODO remove isLinux
-  else if (Platform.isAndroid || Platform.isLinux) {
-    platform = "android";
-  }
-  else {
-    fail('Unsupported platform for screenshots.');
-  }
-
-  // Take screenshot
-  // FIXME args are supported on web only (!!)
-  final args = <String, Object>{
-    'platform': platform,
-    'locale': locale,
-    'device': deviceName,
-    'orientation': orientation,
-  };
-  await takeScreenshot(name, args);
-}
-
-Future<Map<String, dynamic>> takeScreenshot(
-    String screenshot, Map<String, Object?> args) async {
-  integrationTestChannel.setMethodCallHandler(_onMethodChannelCall);
-  final List<int>? rawBytes = await integrationTestChannel.invokeMethod<List<int>>(
-    'captureScreenshot',
-    <String, dynamic>{'name': screenshot, ...args},
-  );
-  if (rawBytes == null) {
-    throw StateError('Expected a list of bytes, but instead captureScreenshot returned null');
-  }
-  return <String, dynamic>{'screenshotName': screenshot, 'bytes': rawBytes};
-}
-
-Future<dynamic> _onMethodChannelCall(MethodCall call) async {
-  switch (call.method) {
-    case 'scheduleFrame':
-      PlatformDispatcher.instance.scheduleFrame();
-  }
-  return null;
 }
--- screenshots/screenshots_driver.dart
+++ screenshots/screenshots_driver.dart
@@ -1,28 +1,9 @@
-import 'dart:io';
-
 import 'package:integration_test/integration_test_driver_extended.dart';
-import 'package:process/process.dart';
+import 'package:screenshots/src/screenshot_handler.dart';
 
 Future<void> main() async {
-  final processManager = LocalProcessManager();
-
   await integrationDriver(
-    onScreenshot: (String screenshotName, List<int> screenshotBytes, [Map<String, Object?>? args]) async {
-      final imageFilename = '.screenshots/$screenshotName.png';
-      final image = await File(imageFilename).create(recursive: true);
-      image.writeAsBytesSync(screenshotBytes);
-
-      final result = await processManager.run([
-        "./screenshots/resources/script/process-screenshot.sh",
-        imageFilename,
-        args!['platform'] as String,
-        args['device'] as String,
-        args['locale'] as String,
-        args['orientation'] as String,
-      ]);
-
-      // Return false if the screenshot is invalid.
-      return (result.exitCode == 0);
-    },
-  );
+      onScreenshot: (String screenshotName, List<int> screenshotBytes,
+              [args]) async =>
+          screenshotHandler(screenshotName, screenshotBytes));
 }

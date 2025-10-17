65acf21ab21a67555c9919bb39c4f11078bcc233
Daniele Ricci <daniele@casaricci.it>
2025-10-17 17:39:35 +0200

    chore: remove unused imports

--- lib/screens/pilot_select/pilot_select_screen.dart
+++ lib/screens/pilot_select/pilot_select_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:provider/provider.dart';
